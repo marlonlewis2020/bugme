47f235f1740e97fea962f1b1e78807dfbddaa827
Marlon Lewis <marlonlewis483@gmail.com>
2021-11-18 03:11:07 -0500

    updated header

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,9 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="2021 BugMe Issue Tracker group assignment for INFO2180 course at the UWI. Year 2 - Web Development">
+        <meta name="keywords" content="issue, issue tracker, bugme, marlon, info2180, 2021, html, css, php, js, sql, ajax">
+        <meta name="author" content="Marlon Lewis, Kathilee Ledgister, Tiffany, Olivia Hibbert, Davica Chambers">
         <title>BugMe Issue Tracker</title>
         <link rel="icon" href="images/group_avatar_icon.png" type="image/x-icon">
         <link rel="stylesheet" href="bugme.css">

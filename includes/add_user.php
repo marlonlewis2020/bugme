34bfc648697472ffed881a0e27846f66717e5ef6
livv8 <oliviahib898@gmail.com>
2021-11-23 18:57:54 -0500

    Took out the header php, css still not working

--- includes/add_user.php
+++ includes/add_user.php
@@ -5,13 +5,12 @@ if(!isset($_SESSION['id'])){
 <?php
 $_SESSION['page']="add_user.php";
 ?>
-<!DOCTYPE html>
-<html lang="en">
+
 <head>
-    <?php include 'header.php'; ?>
-    <link rel="stylesheet" href="bugme2.css">
+     <!--?php include 'header.php'; ?> 
+  <link rel="stylesheet" href="/bugme2.css"> 
 </head>
-<body>
+
     <div class="" id="container">
         <div class="header">
         <header>
@@ -29,27 +28,22 @@ $_SESSION['page']="add_user.php";
 
   <div id="id01" class="modal"> -->
     
-    <form id="user-form" name="user-form" class="modal-content animate" >
-    <!-- CONNECTS TO LOGINACTIONPAGE action="includes/login_action_page.php" method="POST" -->
-      <div class="imgcontainer">
-        <span id="x" class="close" onclick="document.getElementById('id01').style.display='none'" title="Close Login">&times;</span>
-        <img src="images/female_avatar_icon.png" alt="Avatar" class="avatar">
-      </div>
-
-      <div class="container">
+    <form action="add_user_action.php" method="POST" id="add-user-form" name="add-user-form" class="modal-content animate" >
+         <div class="container">
           <div class="form-fields">
-              <label for="firstname"><b>Firstname</b></label>
-              <input id="firstname" type="firstname" placeholder="" name="firstname" required>
+              <label for="firstname"><b>Firstname</b></label><br>
+              <input id="firstname" type="text" placeholder="" name="firstname" required><br>
 
-              <label for="lastname"><b>Lastname</b></label>
-              <input id="lastname" type="lastname" placeholder=" " name="lastname" required>
+              <label for="lastname"><b>Lastname</b></label><br>
+              <input id="lastname" type="text" placeholder=" " name="lastname" required><br>
 
-              <label for="psw"><b>Password</b></label>
-              <input id="psw" type="password" placeholder="" name="psw" required>
+              <label for="psw"><b>Password</b></label><br>
+              <input id="psw" type="password" placeholder="" name="psw" required><br>
 
-              <label for="email"><b>Email</b></label>
-              <input id="email" type="email" placeholder="user@email.com" name="email" required>
-          </div>
+              <label for="email"><b>Email</b></label><br>
+              <input id="email" type="email" placeholder="user@email.com" name="email" required><br>
+              
+          </div><br>
           
         <button id="submit-btn" type="submit">Submit</button>
         <!-- <label>
@@ -57,24 +51,15 @@ $_SESSION['page']="add_user.php";
         </label> -->
       </div>
 
-      <div class="container" style="background-color:#f1f1f1">
-      <!-- <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button> -->
-        <!-- <span class="psw"><a href="#">Forgot password?</a></span> -->
-      </div>
     </form>
   </div>
    
-</body>
-<footer>
-    <div class="" id="footer">
 
-    </div>
-</footer>
-</html>
+
 <?php }
 else{
-  echo "<p>Welcome ".$_SESSION['firstname']." 'login.php'</p>";
-  echo "<p>You are signed in.</p></div>";
+  echo "<p>Welcome ".$_SESSION['firstname']." 'add_user.php'</p>";
+  echo "<p>User Added!</p></div>";
   // include $_SESSION['page'];
 }
 ?>
--- includes/add_user_action.php
+++ includes/add_user_action.php
@@ -5,23 +5,23 @@ require_once 'dbconnect.php';
 $email = filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL);
 $password = filter_input(INPUT_POST,"psw",FILTER_SANITIZE_STRING);
 $firstname=filter_input(INPUT_POST, "firstname",FILTER_SANITIZE_STRING);
-$firstname=filter_input(INPUT_POST, "lastname",FILTER_SANITIZE_STRING);
+$lastname=filter_input(INPUT_POST, "lastname",FILTER_SANITIZE_STRING);
 // var_dump($email,$password);
 
-$sql = "SELECT * FROM `users` WHERE email='".$email."'";
+$sql = "INSERT INTO `users` VALUES($firstname,$lastname, $password, $email )";
 $stmt = $conn->query($sql);
 $results = $stmt->fetch(PDO::FETCH_ASSOC);
 
 if($results && password_verify($password,$results['password'])){
     $_SESSION['firstname'] = $results['firstname'];
-    $_SESSION['id'] = $results['id'];
+    $_SESSION['lastname'] = $results['lastname'];
     $_SESSION['email'] = $results['email'];
-    $_SESSION['page']="home.php";
+    $_SESSION['page']="add_user.php";
 }
 else{
     echo "<p id='error'><i id='warn' class='material-icons'>warning</i> verification failed!</p>";
     $_SESSION['firstname'] = null;
-    $_SESSION['id'] = null;
+    $_SESSION['lastname'] = null;
     $_SESSION['email'] = null;
 }
 
